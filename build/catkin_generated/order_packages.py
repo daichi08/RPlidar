553c1ae853b91460de9ef3f71973cffe9f7083bd
daichi08 <b1504000@planet.kanazawa-it.ac.jp>
2018-06-07 15:22:24 +0900

    can create map with hector_slam

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/daichi/catkin_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/daichi/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/daichi/catkin_ws/devel;/opt/ros/kinetic" != "" else []
