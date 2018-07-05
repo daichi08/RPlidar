84893cc9c6ff47a2e07f0e2d99084e2889eaae85
daichi08 <b1504000@planet.kanazawa-it.ac.jp>
2018-07-05 14:09:03 +0900

    iroiro tuika

---
+++ devel/share/obstacle_detector/cmake/obstacle_detector-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(obstacle_detector_MESSAGE_FILES "/home/daichi/catkin_ws/src/obstacle_detector/msg/CircleObstacle.msg;/home/daichi/catkin_ws/src/obstacle_detector/msg/SegmentObstacle.msg;/home/daichi/catkin_ws/src/obstacle_detector/msg/Obstacles.msg")
+set(obstacle_detector_SERVICE_FILES "")
